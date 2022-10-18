5a0a88433608fbc78c2c0cbd65f1da8971759417
dle234 <108859281+dle234@users.noreply.github.com>
2022-10-18 10:44:43 +0900

    Add files via upload

---
+++ cnu_ai_senti_analysis-main/collector/DaumNewsList.py
@@ -0,0 +1,19 @@
+import pprint
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://news.daum.net/breakingnews/digital'
+result = requests.get(url)
+
+doc = BeautifulSoup(result.text, 'html.parser')  # 텍스트 파일을 html 파일로 읽기
+title_list = doc.select('ul.list_news2 a.link_txt')
+# <a href='url'>:a 태그는 클릭 했을 때 해당 url 로 이동
+# len(): list[]의 갯수를 알려 주는 함수
+# pretty print : pprint
+# pprint.pprint(title_list)
+# print(len(title_list))
+# enumerate(): 반복 하면서 index 번호와 item 을 모두 가져옴
+# list[]의 index 는 0번 부터 시작
+# len(list)= 15, index = 0~14
+for i, title in enumerate(title_list):
+    print(f'인덱스: {i}, 제목: {title.get_text()}')  # []안에 속성값 써주면 그 속성을 가진 요소 가져옴
---
+++ cnu_ai_senti_analysis-main/collector/NaverNews.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://n.news.naver.com/mnews/article/092/0002270275?sid=102'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, liek Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+result = requests.get(url, headers=headers)
+# print(result.text)
+# ctrl+space: 단축키?
+doc = BeautifulSoup(result.text, 'html.parser')
+# strip():앞 뒤 공백 제거
+
+title = doc.select("h2.media_end_head_headline")[0].get_text()
+content = doc.select('div#dic_area')[0].get_text().strip()
+print(f'본문:{title}')  # fstring
+print('내용 : {} '.format(content))  #format 방법
